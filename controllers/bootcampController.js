103966a6235ce6d8bb7bb4a8b76abcc546360d57
tranminhkhang8198 <minhkhang811998@gmail.com>
2020-02-10 14:35:24 +0700

    Controllers: Fetching Bootcamps

--- controllers/bootcampController.js
+++ controllers/bootcampController.js
@@ -3,18 +3,37 @@ const Bootcamp = require('../models/bootcampModel');
 // @desc      Get all bootcamps
 // @route     GET /api/v1/bootcamps
 // @access    Public
-exports.getBootcamps = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        msg: 'Show all bootstrap'
-    });
+exports.getBootcamps = async (req, res, next) => {
+    try {
+        const bootcamps = await Bootcamp.find();
+
+        res.status(200).json({
+            success: true,
+            data: bootcamps
+        });
+    } catch (err) {
+        res.status(400).json({ success: false });
+    }
 }
 
 // @desc      Get single bootcamps
 // @route     GET /api/v1/bootcamps/:id
 // @access    Public
-exports.getBootcamp = (req, res, next) => {
+exports.getBootcamp = async (req, res, next) => {
+    try {
+        const bootcamps = await Bootcamp.find();
 
+        if(!bootcamp) {
+            return res.status(400).json({ success: false });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: bootcamps
+        });
+    } catch (err) {
+        res.status(400).json({ success: false });
+    }
 }
 
 // @desc      Create new bootcamp
