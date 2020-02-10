6d81ea8d1703c55c1387d68877711deabe65acd6
tranminhkhang8198 <minhkhang811998@gmail.com>
2020-02-10 21:11:31 +0700

    Advanced Filtering

--- controllers/bootcampController.js
+++ controllers/bootcampController.js
@@ -8,97 +8,86 @@ const Bootcamp = require('../models/bootcampModel');
 // @route     GET /api/v1/bootcamps
 // @access    Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-    try {
-        const bootcamps = await Bootcamp.find();
+    let query;
 
-        res.status(200).json({
-            success: true,
-            count: bootcamps.length,
-            data: bootcamps
-        });
-    } catch (err) {
-        next(err);
-    }
+    let queryStr = JSON.stringify(req.query);
+
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+    console.log(queryStr);
+
+    query = Bootcamp.find(JSON.parse(queryStr));
+
+    const bootcamps = await query;
+
+    res.status(200).json({
+        success: true,
+        count: bootcamps.length,
+        data: bootcamps
+    });
 });
 
 // @desc      Get single bootcamps
 // @route     GET /api/v1/bootcamps/:id
 // @access    Public
 exports.getBootcamp = asyncHandler(async (req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.findById(req.params.id);
+    const bootcamp = await Bootcamp.findById(req.params.id);
 
-        if(!bootcamp) {
-            next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
-        }
-
-        res.status(200).json({
-            success: true,
-            data: bootcamp
-        });
-    } catch (err) {
-        // next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
-        next(err);
+    if(!bootcamp) {
+        next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
+
+    res.status(200).json({
+        success: true,
+        data: bootcamp
+    });
 });
 
 // @desc      Create new bootcamp
 // @route     POST /api/v1/bootcamps
 // @access    Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.create(req.body);
+    const bootcamp = await Bootcamp.create(req.body);
 
-        res.status(201).json({
-            success: true,
-            data: bootcamp
-        });
-    } catch (err) {
-        next(err);
-    }
+    res.status(201).json({
+        success: true,
+        data: bootcamp
+    });
 });
 
 // @desc      Update bootcamp
 // @route     PUT /api/v1/bootcamps
 // @access    Private
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        });
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
 
-        if (!bootcamp) {
-            next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
-        }
-
-        res.status(200).json({
-            success: true,
-            data: bootcamp
-        });
-    } catch (err) {
-        next(err);
+    if (!bootcamp) {
+        next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
+
+    res.status(200).json({
+        success: true,
+        data: bootcamp
+    });
 });
 
 // @desc      Delete bootcamp
 // @route     DELETE /api/v1/bootcamps/:id
 // @access    Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
-        if (!bootcamp) {
-            next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
-        }
-
-        res.status(200).json({
-            success: true,
-            data: {}
-        });
-    } catch (err) {
-        next(err);
+    if (!bootcamp) {
+        next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
+
+    res.status(200).json({
+        success: true,
+        data: {}
+    });
 });
 
 // @desc      Get bootcamps within a radius
