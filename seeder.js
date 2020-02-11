5fa372463afab4b8d202d2fe05cf3ba83a8f43ad
tranminhkhang8198 <minhkhang811998@gmail.com>
2020-02-11 17:45:01 +0700

    Bootcamp User Relationship

--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({ path: './config/config.env' });
 // Load models
 const Bootcamp = require('./models/bootcampModel');
 const Course = require('./models/courseModel');
+const User = require('./models/userModel');
 
 // Connect to DB
 const DB = process.env.DATABASE.replace(
@@ -27,8 +28,13 @@ mongoose.connect(DB, {
 const bootcamps = JSON.parse(
     fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
+
 const courses = JSON.parse(
     fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
+
+const users = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
 );
 
 // Import into DB
@@ -36,6 +42,7 @@ const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
         await Course.create(courses);
+        await User.create(users);
 
         console.log('Data Imported...'.green.inverse);
         process.exit();
@@ -47,7 +54,9 @@ const importData = async () => {
 // Delete data
 const deleteData = async () => {
     try {
-        await Bootcamp.deleteMany(); await Course.deleteMany();
+        await Bootcamp.deleteMany();
+        await Course.deleteMany();
+        await User.deleteMany();
 
         console.log('Data Destroyed...'.red.bold.inverse);
         process.exit();
